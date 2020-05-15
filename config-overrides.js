fc918b1683f37793ace44457645a06eff1e7fc81
xingchuan <xingchuan@byai.com>
2020-05-15 18:14:46 +0800

    docs: format文件格式

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const path = require('path');
 const {
   override,
-  addBabelPlugin,
+  addBabelPlugin
 } = require('customize-cra');
 
 // 覆写「入口|出口」配置
-const overrideEntryAndOutputConfig = () => config => {
+const overrideEntryAndOutputConfig = () => (config) => {
   if (config.mode === 'production') {
     config = {
       ...config,
@@ -14,46 +14,48 @@ const overrideEntryAndOutputConfig = () => config => {
         ...config.output,
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        chunkFilename: 'chunks/[name].[contenthash:8].chunk.js'
-      }
-    }
-  }
-  return config;
-}
-
-// 移除指定plugins
-const removePlugins = (plugins) => config => {
-  if (config.mode === 'production') {
-    config.plugins = config.plugins.filter(
-      p => {
-        const pluginName = p.constructor.name;
-        return Array.isArray(plugins) ? !plugins.includes(pluginName) : typeof pluginName === 'string' ? pluginName !== plugins : true
-      }
-    );
+        chunkFilename: 'chunks/[name].[contenthash:8].chunk.js',
+      },
+    };
   }
   return config;
 };
 
+// 移除指定plugins
+const removePlugins = (plugins) => (config) => {
+  if (config.mode === 'production') {
+    config.plugins = config.plugins.filter((p) => {
+      const pluginName = p.constructor.name;
+      return Array.isArray(plugins) ?
+        !plugins.includes(pluginName) :
+        typeof pluginName === 'string' ?
+        pluginName !== plugins :
+        true;
+    });
+  }
+  return config;
+};
 
-const addBabelLoaderRule = () => config => {
+const addBabelLoaderRule = () => (config) => {
   config.module.rules.unshift({
     test: /\.(ts|tsx)$/,
     use: [{
       loader: require.resolve('babel-loader'),
       options: {
         presets: [
-          ['react-app', {
-            flow: false,
-            typescript: true
-          }]
+          [
+            'react-app',
+            {
+              flow: false,
+              typescript: true,
+            },
+          ],
         ],
       },
     }, ],
   });
   return config;
 };
-
-
 
 module.exports = {
   // The Webpack config to use when compiling your react app for development or production.
